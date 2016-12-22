ebe4bd2285ab545bbf80d3514f52992f30421f67
IgorYang <yanggang1025@163.com>
2016-12-22 13:06:56 +0800

    fix title

---
+++ learning20161222/login.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+#author:igor
+#date:20161222
+
+
+def login(username,password):
+    pass
+
+def register(username,password)
+    pass
+
+def main():
+    pass
+
+main()
