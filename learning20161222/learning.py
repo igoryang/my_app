96fb7762dd7a0660b0baabe0fed5ae8fd9adfa89
IgorYang <yanggang1025@163.com>
2016-12-22 15:44:12 +0800

    fix title

---
+++ learning20161222/learning.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+#author = 'igor'
+''''''
+"""
+变量
+可变的量
+命名：字母，数字，下划线   不能以数字开头，区分大小写
+number = 3    #把3 赋值给变量number
+
+常量：全部大写
+NAME
+全局变量 global 重新赋值
+全部大写
+
+
+if 3==3:
+    pass
+else:
+    pass
+if  elif:  elif: ...else:
+
+for i in rang(10):
+
+while Ture:
+    continue
+    break
+
+
+函数：
+1，def
+2,名称
+3，函数体
+4,参数：
+    普通参数
+    指定参数
+    动态参数
+       *args    列表传输
+       *kwargs
+
+       **args   字典传输
+       **kwargs
+     万能参数
+def f1(*args):
+     pass
+
+format
+print ( "this is format 0 {0},this is format 1 {1}".format(112,222))
+
+三元运算  | 三木运算
+name = '3' if 1==1 else '4'
+print(name)
+
+
+"""
+
