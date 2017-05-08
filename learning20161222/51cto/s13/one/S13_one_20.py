c680380a317823fd76e8dd9571a7f8bcdf1ba287
igoryang <yanggang1025@163.com>
2017-05-08 23:06:07 +0800

    51cto

--- learning20161222/51cto/s13/one/S13_one_20.py
+++ learning20161222/51cto/s13/one/S13_one_20.py
@@ -6,11 +6,24 @@ __author__ = 'YangGang'
 
 '''
 作业查询
-
-/crm/grade/single/
+http://www.oldboyedu.com/
+http:oldboyedu.com/crm/grade/single/
+284526658 stu177851
 
 
 be a new gentlmen
+
+关闭得体
+文明礼让
+多去旅行，一年至少一个国家
+多看书，电影，一年15本书，50+电影
+学好英语
+保持不甘心，保持正能量
+做事情一定要换位思考，要想此事会不会给别人造成不好的困扰
+积极影响身边的人
+
+三体
+百年孤独
 
 
 '''
