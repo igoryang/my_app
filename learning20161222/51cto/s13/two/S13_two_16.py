8bc96a4820fefac66f380b6d7dd827e1b54c6522
igoryang <yanggang1025@163.com>
2017-05-22 22:31:34 +0800

    51cto s13 two 16

--- learning20161222/51cto/s13/two/S13_two_16.py
+++ learning20161222/51cto/s13/two/S13_two_16.py
@@ -18,7 +18,19 @@ print(n)
 
 #字节转换成字符串
 str()
+s = "李杰"
 s = str(bytes("李杰",encoding="utf-8"),encoding="utf-8")
 print(s)
 s1 = str(bytes(s,encoding="utf-8"),encoding="utf-8")
 print(s1)
+
+s2 = b'\xe6\x9d\x8e\xe6\x9d\xb0'
+print(s2.decode('utf-8'))
+# print(str(bytes(s2,encoding="utf-8"),encoding="utf-8"))
+
+# name = 'zifuchuang'
+# nameBytes = name.encode('utf-8')   # 字节
+# nameStr = nameBytes.decode('utf-8')# 字符串
+# print(name)
+# print(nameBytes)
+# print(nameStr)
