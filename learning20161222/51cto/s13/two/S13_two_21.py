be006b09c73b7a2bc52bc01c566871ac3fc004f6
igoryang <yanggang1025@163.com>
2017-05-23 22:27:13 +0800

    51cto s13 two 21  文件操作 HA

--- learning20161222/51cto/s13/two/S13_two_21.py
+++ learning20161222/51cto/s13/two/S13_two_21.py
@@ -42,4 +42,30 @@ with open('ha1','r',encoding="utf-8") as f1,open('ha2','w',encoding="utf-8") as 
         f2.write(new_str)
 
 
+1，获取HA记录   如输入：www.oldboy.org
+2，添加HA记录   如： {"backend": "test.oldboy.org","record":{"server": "100.1.7.9","weight": 20,"maxconn": 30}}
+3，删除HA记录  如： {"backend": "test.oldboy.org","record":{"server": "100.1.7.9","weight": 20,"maxconn": 30}}
+
+
+
+
+如果想要将用户输入的内容转换下格式，
+
+1
+2
+'{"k1":123, "k2": "wupeiqi"}'       字符串转换成字典类型
+"[11,22,33,44]"                     字符串转换成列表类型
+################# 当当当当，python的json模块可以搞搞搞 !!!!!!! #################
+
+json模块
+json模块的作用之一：根据字符串书写格式，将字符串自动转换成相应格式
+
+import json
+inp_str = "[11,22,33,44]"
+inp_list = json.loads(inp_str) # 根据字符串书写格式，将字符串自动转换成 列表类型
+
+
+inp_str = ' {"k1":123, "k2": "wupeiqi"} '  # 正确的输入      切记，内部必须是 双引号 ！！！
+#inp_str = " {'k1':123, 'k2': 'wupeiqi'}"   # 错误的输入
+inp_dict = json.loads(inp_str) # 根据字符串书写格式，将字符串自动转换成 字典类型
 '''
