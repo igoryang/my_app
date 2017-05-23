3bc9ae536942d69e19b31c93b19c3876c6fefd91
gang.yang <gang.yang@china.zhaogang.com>
2017-05-23 09:54:13 +0800

    split_join.py

---
+++ learning20161222/51cto/s13/two/split_join.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding=utf-8 -*-
+# -*- coding:utf-8 -*-
+# author Y.G
+__author__ = 'YangGang'
+
+
+
+name = "ab cd efg"
+name2 = name.split(" ")
+print(name)
+print(name2)
+
+name3 = ["a","b","c"]
+name4 = ("|").join(name3)
+print(name3)
+print(name4)
