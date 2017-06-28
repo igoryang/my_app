e80c0ccbed9da98dda87b00b0c42d51086ba7e2c
igoryang <yanggang1025@163.com>
2017-06-28 21:59:15 +0800

    装饰器

--- learning20161222/51cto/s13/two/S13_two_40_2-12.py
+++ learning20161222/51cto/s13/two/S13_two_40_2-12.py
@@ -3,3 +3,22 @@
 # -*- coding:utf-8 -*-
 # author Y.G
 __author__ = 'YangGang'
+
+"""
+1，python基础  变量，常量 for  if elif else  while  in 
+
+2，python基本数据类型 int str list tuple dict set  -->类
+
+    li = [11,22,33] 对象
+    s = "xyz"
+3，函数式编程
+
+4，装饰器  本身是函数
+  @装饰器函数名
+  def func():
+     pass
+     
+   1,把 func体赋值到装饰器,并执行
+   2，装饰器的 返回值重新赋值给func
+
+"""
--- learning20161222/51cto/s13/two/S13_two_40_2-8.py
+++ learning20161222/51cto/s13/two/S13_two_40_2-8.py
@@ -16,7 +16,9 @@ __author__ = 'YangGang'
 
 '''
 
-LOGIN_USER = {"is_login":False,"courrent_user":user}
+'''
+#方案一
+LOGIN_USER = {"is_login":False}
 
 def outer(func):
     def inner(*args,**kwargs):
@@ -27,33 +29,89 @@ def outer(func):
             print('请登陆')
     return inner
 
+def orde():
+    if LOGIN_USER['is_login']:
+        print('欢迎%s登陆'%LOGIN_USER['current_user'])
+    else:
+        print('请登陆')
+
 def changpwd():
-    pass
+    if LOGIN_USER['is_login']:
+        print('欢迎%s登陆'%LOGIN_USER['current_user'])
+    else:
+        print('请登陆')
 
 
 def manager():
     if LOGIN_USER['is_login']:
-        print('欢迎%s登陆') %LOGIN_USER['current_user']
+        print('欢迎%s登陆'%LOGIN_USER['current_user'])
     else:
         print('请登陆')
+
 
 def login(user,pwd):
     if user == "alex" and pwd == "123":
         LOGIN_USER['is_login'] = True
         LOGIN_USER['current_user'] = user
         manager()
-    pass
+    else:
+        print("请登录")
 
 def main():
-    input("1,后台管理 ; 2,登录")
-    if inp == '1':
-        manager()
-    elif inp == '2':
-        username = input("username:")
-        pwd = input("password:")
-        login(username,pwd)
+    while True:
+        inp = input("1,后台管理 ; 2,登录")
+        if inp == '1':
+            manager()
+        elif inp == '2':
+            username = input("username:")
+            pwd = input("password:")
+            login(username,pwd)
+main()
 
+'''
+#方案二 使用装饰器
+LOGIN_USER = {'is_login':False}
+#装饰器 权限控制 outer
+def outer(func):
+    def inner():
+        if LOGIN_USER['is_login']:
+            r = func()
+            return r
+        else:
+            print("请登录哦")
+    return inner
 
+@outer
+def order():
+    print("欢迎%s登录"% LOGIN_USER['current_user'])
+
+@outer
+def changepwd():
+    print("欢迎%s登录" % LOGIN_USER['current_user'])
+
+@outer
+def manage():
+    print("欢迎%s登录" % LOGIN_USER['current_user'])
+
+def login(user,pwd):
+    if user == "alex" and pwd == "123":
+        LOGIN_USER['is_login'] = True
+        LOGIN_USER['current_user'] = user
+        manage()
+    else:
+        print("请登录哦！")
+
+def main():
+    while True:
+        inp = input("1:登录后台  2，登录")
+        if inp == '1':
+            manage()
+        elif inp == '2':
+            username = input("请输入用户名：")
+            pwd = input("请输入用户密码：")
+            login(username,pwd)
+
+main()
 
 '''
 def outer(func):
